520018189902037a1a0e61cc3da8916223be0bf0
Fabio Avila <fabioravila@gmail.com>
2017-01-15 18:05:12 -0200

    initial project commit

---
+++ AppVeyor.Playground/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppVeyor.Playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Just a sample app tp test AppVeyor services");
+            Console.ReadKey();
+        }
+    }
+}
